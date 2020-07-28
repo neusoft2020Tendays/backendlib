07199578a992410073dfb3babbe51270b8e7ac15
霸晨民 <1170300524@stu.hit.edu.cn>
2020-07-28 12:08:07 +0800

    霸晨民 增加了员工部分的增删改查，已测试

--- src/main/java/com/hitstu/oa/test/Test.java
+++ src/main/java/com/hitstu/oa/test/Test.java
@@ -1,25 +1,42 @@
 package com.hitstu.oa.test;
 
-import java.util.List;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.hitstu.oa.checkin.model.BedModel;
-import com.hitstu.oa.checkin.service.IBedService;
 import com.hitstu.oa.config.SpringConfigForSM;
+import com.hitstu.oa.hr.model.DepartmentModel;
+import com.hitstu.oa.hr.model.EmployeeModel;
+import com.hitstu.oa.hr.service.IEmployeeService;
+
 
 public class Test {
+	private static EmployeeModel getEmployeeModel(String id, String deptid, String name, String sex, int age, String fired, String leave) {
+		EmployeeModel employeeModel = new EmployeeModel();
+		DepartmentModel departmentModel = new DepartmentModel();
+		departmentModel.setDeptid(deptid);
+		
+		employeeModel.setId(id);
+		employeeModel.setDepartment(departmentModel);
+		employeeModel.setName(name);
+		employeeModel.setSex(sex);
+		employeeModel.setAge(age);
+		employeeModel.setFired(fired);
+		employeeModel.setLeave(leave);
+		
+		return employeeModel;
+	}
+	
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigForSM.class);
-		IBedService bedService = context.getBean(IBedService.class);
-
+		IEmployeeService employeeService = context.getBean(IEmployeeService.class);
 		try {
-			List<BedModel> list = bedService.getByAllWithPage(3, 3);
-			for (BedModel bedModel : list) {
-				System.out.println(bedModel.getId());
-			}
-			
+			System.out.println(employeeService.getById("1705004").getName());
+			EmployeeModel employeeModel = getEmployeeModel(
+					"1705004", "D05", "孙悟空", "男", 27, "否", "否");
+			employeeService.modify(employeeModel);
+			System.out.println(employeeService.getById("1705004").getName());
+			employeeService.delete(employeeModel);
+			System.out.println(employeeService.getByAll().size());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
