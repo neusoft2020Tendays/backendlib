399198dca75d623b5a957d1ef22d5c9ffd51ef3f
王航 <1913066550@qq.com>
2020-07-29 23:52:37 +0800

    王航：修改一点小bug

--- src/main/java/com/hitstu/oa/checkin/controller/CostController.java
+++ src/main/java/com/hitstu/oa/checkin/controller/CostController.java
@@ -1,6 +1,7 @@
 package com.hitstu.oa.checkin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import com.hitstu.oa.restresult.Result;
 
 @RestController
 @RequestMapping(value = "/cost")
+@CrossOrigin(origins = { "*", "null" })
 public class CostController {
 	
 	@Autowired
