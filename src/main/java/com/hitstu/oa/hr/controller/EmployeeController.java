6d69c57dfb5f4b115d41a80b9fd5fe6de2fff2d1
霸晨民 <1170300524@stu.hit.edu.cn>
2020-07-29 09:37:17 +0800

    霸晨民：修改增加了employee Controller层功能

--- src/main/java/com/hitstu/oa/hr/controller/EmployeeController.java
+++ src/main/java/com/hitstu/oa/hr/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hitstu.oa.checkin.model.BedModel;
 import com.hitstu.oa.hr.model.EmployeeModel;
 import com.hitstu.oa.hr.service.IEmployeeService;
 import com.hitstu.oa.restresult.Result;
@@ -20,13 +21,74 @@ import com.hitstu.oa.restresult.Result;
 @ResponseBody
 public class EmployeeController {
 	@Autowired
-	private IEmployeeService employeeServiceeService = null;
+	private IEmployeeService employeeService = null;
 	
+	@PostMapping("/add")
+	public Result<String> add(@RequestBody EmployeeModel employeeModel) throws Exception {
+		employeeService.add(employeeModel);
+		Result<String> result = new Result<>();
+		result.setStatus("OK");
+		result.setMessage("增加员工成功");
+		return result;
+	}
+	
+	@PostMapping("/modify")
+	public Result<String> modify(@RequestBody EmployeeModel employeeModel) throws Exception {
+		employeeService.modify(employeeModel);
+		Result<String> result = new Result<>();
+		result.setStatus("OK");
+		result.setMessage("修改员工成功");
+		return result;
+	}
+	
+	@PostMapping("/delete")
+	public Result<String> delete(@RequestBody EmployeeModel employeeModel) throws Exception {
+		employeeService.delete(employeeModel);
+		Result<String> result = new Result<>();
+		result.setStatus("OK");
+		result.setMessage("删除员工成功");
+		return result;
+	}
+	
+	@GetMapping("/listAll")
+	public Result<EmployeeModel> getListByAll() throws Exception {
+		Result<EmployeeModel> result = new Result<>();
+		result.setCount(employeeService.getCountByAll());
+		result.setList(employeeService.getByAll());
+		result.setStatus("OK");
+		result.setMessage("取得全部床位成功");
+		return result;
+	}
+
+	@GetMapping("/listAllPage")
+	public Result<EmployeeModel> getListByAllWithPage(@RequestParam(required = false, defaultValue = "10") int rows,
+			@RequestParam(required = false, defaultValue = "1") int page) throws Exception {
+		
+		Result<EmployeeModel> result = new Result<>();
+		result.setCount(employeeService.getCountByAll());
+		result.setPage(employeeService.getPageCountByAll(rows));
+		result.setRows(rows);
+		result.setPage(page);
+		result.setList(employeeService.getByAllWithPage(rows, page));
+		result.setStatus("OK");
+		result.setMessage("取得分页床位成功");
+		return result;
+	}
+	
+	@GetMapping("/get")
+	public Result<EmployeeModel> getById(@RequestParam(required = true) String id) throws Exception {
+		
+		Result<EmployeeModel> result = new Result<>();
+		result.setResult(employeeService.getById(id));
+		result.setStatus("OK");
+		result.setMessage("取得特定床位成功");
+		return result;
+	}
 	
 	@GetMapping("/getCountByDepartment")
 	public Result<Integer> getCountByDepartment(@RequestParam(required = true) String deptid) throws Exception {
 		Result<Integer> result = new Result<>();
-		result.setResult(employeeServiceeService.getCountByDepartment(deptid));
+		result.setIntResult(employeeService.getCountByDepartment(deptid));
 		result.setStatus("OK");
 		result.setMessage("根据部门ID获取员工数成功");
 		return result;
