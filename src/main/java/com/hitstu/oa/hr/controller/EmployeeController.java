e454cfa3a1ace1799e32744fd7f553df6180c842
霸晨民 <1170300524@stu.hit.edu.cn>
2020-07-29 09:25:12 +0800

    霸晨民 修改了返回值

--- src/main/java/com/hitstu/oa/hr/controller/EmployeeController.java
+++ src/main/java/com/hitstu/oa/hr/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.hitstu.oa.hr.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,10 +22,12 @@ public class EmployeeController {
 	@Autowired
 	private IEmployeeService employeeServiceeService = null;
 	
+	
 	@GetMapping("/getCountByDepartment")
 	public Result<Integer> getCountByDepartment(@RequestParam(required = true) String deptid) throws Exception {
 		Result<Integer> result = new Result<>();
 		result.setResult(employeeServiceeService.getCountByDepartment(deptid));
+		result.setStatus("OK");
 		result.setMessage("根据部门ID获取员工数成功");
 		return result;
 	}
